8e717ede12c6905e9de66a21cdf0df6bab3de906
Diego Alejandro Galdamez Tinoco <diego.galdamez@unitec.edu>
2016-10-21 14:26:30 -0600

    Programa

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main(){
 
 	do{
 		int opcion;
+		cout << "1. Ejercicio 1" << "\n" << "2. Ejercicio 2" << "\n" << "3.Ejercicio 3" << "\n";
 		cout << "Ingrese una opcion: ";
 		switch(opcion){
 			case 1:{
@@ -29,6 +30,34 @@ int main(){
 		}
 	}
 
+}
+
+
+void protoEjercicio2 (int numero){
+	int divisores[15];
+	int totalNumeros = 0;
+	int location = 0;
+	for (int i = 1; i <= numero; ++i)
+	{
+		if(numero % i == 0){
+			divisores[location] = i;
+			location++;
+		}
+	}//fin de for
+
+	for (int i = 0; i < location; ++i)
+	{
+		cout << "Los divisores son : " << divisores[location] << "\n";
+	}
 
 }
 
+void isPrime(int numero){
+	int dividendos = 0;
+	for (int i = 0; i < count; ++i)
+		{
+			
+		}	
+
+
+}
