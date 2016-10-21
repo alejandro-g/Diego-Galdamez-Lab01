98ffa69094547a72e34b65de3f31cb6c75305bcb
Diego Alejandro Galdamez Tinoco <diego.galdamez@unitec.edu>
2016-10-21 13:41:15 -0600

    Programa

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void protoEjercicio1();
+
+void protoEjercicio2(int);
+
+void protoEjercicio3();
+
+int main(){
+
+	do{
+		int opcion;
+		cout << "Ingrese una opcion: ";
+		switch(opcion){
+			case 1:{
+
+				break;
+			}
+			
+			case 2:{
+				break;
+			}
+			
+			case 3:{
+				break;
+			}
+		}
+	}
+
+
+}
+
